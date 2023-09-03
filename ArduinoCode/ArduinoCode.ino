0a72c9ff6face161cf64c3ec76621d371c8575ce
AminSheykhzadeh <amin.sh.sos@gmail.com>
2023-09-03 15:09:31 +0330

    file name changed

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,7 +1,7 @@
 void setup() {
   // put your setup code here, to run once:
-
-}
+  Serial.begin(115200);
+  Serial.println("hello world!");}
 
 void loop() {
   // put your main code here, to run repeatedly:
